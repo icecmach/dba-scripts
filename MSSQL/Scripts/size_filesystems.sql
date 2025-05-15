14e076c9711457c916477f7be2ca99da4fc2421c
Andre Penedo <andre.penedo@cgi.com>
2025-05-15 14:33:00 -0400

    add scripts

--- MSSQL/Scripts/size_filesystems.sql
+++ MSSQL/Scripts/size_filesystems.sql
@@ -1,6 +1,7 @@
 SELECT name,
     s.used / 128.0 AS SpaceUsedInMB,
-    size / 128.0 - s.used / 128.0 AS AvailableSpaceInMB
+    size / 128.0 - s.used / 128.0 AS AvailableSpaceInMB,
+	(CAST(used AS FLOAT) / CAST(size AS FLOAT)) * 100 AS SpaceUsedInPercent
 FROM sys.database_files
 CROSS APPLY (SELECT CAST(FILEPROPERTY(name, 'SpaceUsed') AS INT)) s(used)
 WHERE FILEPROPERTY(name, 'SpaceUsed') IS NOT NULL;
